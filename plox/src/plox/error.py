414e1118902769b0ada2cbfbd76e65263ab71d94
Z9RO@lm <z9ro4github@outlook.com>
2025-03-30 13:38:15 +0800

    Fix: local variable

--- plox/src/plox/error.py
+++ plox/src/plox/error.py
@@ -1,21 +1,23 @@
 import sys
 
 
-__has_error = False
+_has_error = False
 
 
 def has_error() -> bool:
-    return __has_error
+    return _has_error
 
 
 def reset() -> None:
-    __has_error = False
+    global _has_error
+    _has_error = False
 
 
 def error(line: int, message: str) -> None:
-    __report(line, "", message)
+    _report(line, "", message)
 
 
-def __report(line: int, where: str, message: str) -> None:
+def _report(line: int, where: str, message: str) -> None:
+    global _has_error
     print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
-    __has_error = True
+    _has_error = True
