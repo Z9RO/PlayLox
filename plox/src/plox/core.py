450365d9a450d525b459f1ec82f987fa0623b9f0
Z9RO@lm <z9ro4github@outlook.com>
2025-03-30 13:46:56 +0800

    add basic function into core

--- plox/src/plox/core.py
+++ plox/src/plox/core.py
@@ -1,16 +1,31 @@
-def runFile(source: str) -> None:
-    print(f"run source file {source}")
+from sys import stdin
+from plox.error import reset
+from plox.scanner import Scanner
+
+def _run(source: str) -> None:
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+    for token in tokens:
+        print(token)
+
+def _run_file(source_file: str) -> None:
+    with open(source_file, "r") as f:
+        content = f.read()
+        _run(content)
 
 
-def runPrompt() -> None:
-    print("run prompt")
+def _run_prompt() -> None:
+    for line in stdin:
+        reset()
+        _run(line)
+
 
 
 def main(args: list[str] | None = None) -> None:
     if (args is None) or len(args) == 0:
-        runPrompt()
+        _run_prompt()
     elif len(args) == 1:
-        runFile(args[0])
+        _run_file(args[0])
     else:
         print("Usage: plox [script]")
         exit(64)
